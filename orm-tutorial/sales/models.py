afdaeaefc7c872f02dfca233086d510bb5c5cb9c
Austin Shen <austin.shen@csiro.au>
2021-02-25 12:54:22 +0800

    clean db, clean migratiosn, first draft tutorial

--- orm-tutorial/sales/models.py
+++ orm-tutorial/sales/models.py
@@ -7,4 +7,4 @@ class Sale(models.Model):
     id = models.AutoField(primary_key=True)
     book = models.ForeignKey(Book, to_field='id', on_delete=models.DO_NOTHING)
     store = models.ForeignKey(Store, to_field='id', on_delete=models.DO_NOTHING)
-    sold_at = models.DateTimeField(auto_now=True)
+    sold_at = models.DateTimeField()
