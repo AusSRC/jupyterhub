a4af750e4e3bd09d48a23b01799c34db78550417
Austin Shen <austin.shen@csiro.au>
2021-03-02 13:55:17 +0800

    wallaby notebook moved here

---
+++ wallaby_notebook/instance/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from run.models import Run
+
+
+class Instance(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    run = models.ForeignKey(Run, models.DO_NOTHING)
+    filename = models.CharField(max_length=200)
+    boundary = models.TextField()
+    run_date = models.DateTimeField()
+    flag_log = models.BinaryField(blank=True, null=True)
+    reliability_plot = models.BinaryField(blank=True, null=True)
+    log = models.BinaryField(blank=True, null=True)
+    parameters = models.JSONField()
+    version = models.CharField(max_length=200, blank=True, null=True)
+    return_code = models.IntegerField(blank=True, null=True)
+    stdout = models.BinaryField(blank=True, null=True)
+    stderr = models.BinaryField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'instance'
+        unique_together = (('run', 'filename', 'boundary'),)
