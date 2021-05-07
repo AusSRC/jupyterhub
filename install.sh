30d55210bef21ad0af1fbf6538ae014b8c89133f
Austin Shen <austin.shen@csiro.au>
2021-05-07 13:37:19 +0800

    move install to infrastructure

--- install.sh
+++
@@ -1,4 +0,0 @@
-export KUBECONFIG=$(pwd)/kubeconfig_aussrc-radec-cluster
-helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
-helm repo update
-helm install jupyterhub jupyterhub/jupyterhub -f jupyterhub/config.yml -n jupyter --timeout 500s
